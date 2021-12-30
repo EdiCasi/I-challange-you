38adb7eaabd31b6e392fba83b9a0699f327fc63c
Galca Vlad-Daniel <vladgalca@gmail.com>
2021-12-30 14:09:25 +0200

    Added Notifications Button
    Display currently not tied to actual procedure

---
+++ I challenge you 3.0/DataAccessLayers/NotificationDAL.cs
@@ -0,0 +1,35 @@
+﻿using I_challenge_you_3._0.DataAccessLayer;
+using I_challenge_you_3._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_challenge_you_3._0.DataAccessLayers
+{
+    class NotificationDAL
+    {
+        public static int GetNotificationCount(int userId)
+        {
+            using (SqlConnection con = DALHelper.Connection)
+            {
+                SqlCommand cmd = new SqlCommand("getNotifCount", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@userId", SqlDbType.VarChar).Value = userId;
+                con.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    return (int)reader["userId"];
+                }
+                reader.Close();
+
+            }
+            return 0;
+        }
+    }
+}
